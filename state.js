9fef6c5320bb9953d1b25b77259f1f3ff7a258d4
Fahri <desrizalfahri@gmail.com>
2024-03-09 14:00:09 +0700

    [NEW] learning js

---
+++ state.js
@@ -0,0 +1,22 @@
+/*
+const coffeeStock = {
+    arabica: 100,
+    robusta: 150,
+    liberica: 200
+};
+
+//module.exports = coffeeStock;
+const isCoffeeMachineReady = true;
+
+module.exports = {coffeeStock, isCoffeeMachineReady};
+*/
+
+const coffeeStock = {
+    arabica: 100,
+    robusta: 150,
+    liberica: 200
+  };
+
+const isCoffeeMachineReady = true;
+   
+export {coffeeStock, isCoffeeMachineReady};
